8e7cf4805da1a3eb8cc09a9f937a955def153cc0
anton-k <anton.kholomiov@gmail.com>
2018-01-31 12:42:07 +0300

    Makes it work with stack. Updates deprecated parseUrl

--- src/Bitfinex/Core.hs
+++ src/Bitfinex/Core.hs
@@ -24,7 +24,7 @@ decodeBody = decode . responseBody
 -- | Gets a list of available "Symbol"s with their associated details.
 getSymbolsDetails :: IO (Either String [Symbol])
 getSymbolsDetails = do
-    request <- parseUrl "https://api.bitfinex.com/v1/symbols_details"
+    request <- parseRequest "https://api.bitfinex.com/v1/symbols_details"
     m <- makeManager
     a <- httpLbs request m
     let b = decodeBody a
@@ -35,7 +35,7 @@ getSymbolsDetails = do
 -- | Gets a list of available "Ticker"s.
 getSymbols :: IO (Either String [Ticker])
 getSymbols = do
-    request <- parseUrl "https://api.bitfinex.com/v1/symbols"
+    request <- parseRequest "https://api.bitfinex.com/v1/symbols"
     m <- makeManager
     a <- httpLbs request m
     let b = decodeBody a
@@ -46,7 +46,7 @@ getSymbols = do
 -- | Get the most recent "TickerData" for a "Ticker". Contains lots of price data.
 getTicker :: Ticker -> IO (Either String TickerData)
 getTicker t = do
-    request <- parseUrl $ "https://api.bitfinex.com/v1/pubticker/" ++ unTicker t
+    request <- parseRequest $ "https://api.bitfinex.com/v1/pubticker/" ++ unTicker t
     m <- makeManager
     a <- httpLbs request m
     let b = decodeBody a
@@ -57,7 +57,7 @@ getTicker t = do
 -- | Get a list of the most recent "Stats" of a "Ticker". Contains volume and period.
 getStats :: Ticker -> IO (Either String [Stats])
 getStats t = do
-    request <- parseUrl $ "https://api.bitfinex.com/v1/stats/" ++ unTicker t
+    request <- parseRequest $ "https://api.bitfinex.com/v1/stats/" ++ unTicker t
     m <- makeManager
     a <- httpLbs request m
     let b = decodeBody a
@@ -68,7 +68,7 @@ getStats t = do
 -- | Get the most recent "FundingBook" of a "Currency".
 getFundingBook :: Currency -> IO (Either String FundingBook)
 getFundingBook c = do
-    request <- parseUrl $ "https://api.bitfinex.com/v1/lendbook/" ++ unCurr c
+    request <- parseRequest $ "https://api.bitfinex.com/v1/lendbook/" ++ unCurr c
     m <- makeManager
     a <- httpLbs request m
     let b = decodeBody a
@@ -79,7 +79,7 @@ getFundingBook c = do
 -- | Get the most recent "OrderBook" of a "Ticker"
 getOrderBook :: Ticker -> IO (Either String OrderBook)
 getOrderBook c = do
-    request <- parseUrl $ "https://api.bitfinex.com/v1/book/" ++ unTicker c
+    request <- parseRequest $ "https://api.bitfinex.com/v1/book/" ++ unTicker c
     m <- makeManager
     a <- httpLbs request m
     let b = decodeBody a
@@ -90,7 +90,7 @@ getOrderBook c = do
 -- | Get list of all recent "Trade"s of a "Ticker". Returns an error string if fails.
 getTrades :: Ticker -> IO (Either String [Trade])
 getTrades t = do
-    request <- parseUrl $ "https://api.bitfinex.com/v1/trades/" ++ unTicker t
+    request <- parseRequest $ "https://api.bitfinex.com/v1/trades/" ++ unTicker t
     m <- makeManager
     a <- httpLbs request m
     let b = decodeBody a
@@ -101,7 +101,7 @@ getTrades t = do
 -- | Get list of all recent "Loan"s: total amount lent and FRR in % by 365 days.
 getLoans :: Currency -> IO (Either String [Loan])
 getLoans c = do
-    request <- parseUrl $ "https://api.bitfinex.com/v1/lends/" ++ unCurr c
+    request <- parseRequest $ "https://api.bitfinex.com/v1/lends/" ++ unCurr c
     m <- makeManager
     a <- httpLbs request m
     let b = decodeBody a
